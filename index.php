481f5436b3a95e9de23e76026dfca2f351391b81
Bastodon <superbastodon@gmail.com>
2024-03-27 11:33:56 +0100

    Début de la mise en forme du FRONT. Ajout du header et du style.css

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
 <!DOCTYPE html>
-<html>
 <html lang="fr">
+
   <head>
     <meta charset="utf-8">
     <title>Groppo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> </script>
-    <link rel="stylesheet" href="styles.css" type="text/css">
+    <link rel="stylesheet" href="style.css" type="text/css">
     <link rel="icon" type="image/x-icon" href="assets/grop.ico">
   </head>
+
     <body>
-        <?php echo '<p>Hello world</p>'; ?>
+        
+        <header>
+            <a href="index.php"> <img src="assets/grop.ico" alt="logo"> </a>
+            <a href="index.php"> <h1> Groppo </h1> </a>
+            <ul>
+                <li> Rechercher </li>
+            </ul>
+        </header>
+
+        <div class="contact-main">
+
+        </div>
+
+        <footer>
+            <h4> Groppo, par Thomas LERAY 2024 </h4>
+        </footer>
+
     </body>
+
 </html>
