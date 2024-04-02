982480dd09632fb16b2a6d90445de3dd1e67f423
Bastodon <superbastodon@gmail.com>
2024-04-02 12:30:54 +0200

    Correction connexion BDD

--- db.php
+++ db.php
@@ -1,14 +1,8 @@
 <?php
 
-    $con = mysqli_connect("localhost","root","toor","sql_groppo");
+    $con = mysqli_connect("localhost","root","","sql_groppo");
 
     if (mysqli_connect_errno()) {
         printf("", mysqli_connect_error());
     }
-
-    if ($con) {
-        die("Erreur de connexion : " .$con->connect_error);
-    }
-
-    echo"Connexion reussie";
 ?>
